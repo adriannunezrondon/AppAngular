6a42a12eca3a054260a7d81b54fea6441dfeec2a
adrian <adriannunezrondon@gmail.com>
2020-03-06 14:48:27 -0300

    Eventos y Formularios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-   name: string='Ryan Ray';
-  age:number;
-  address:{
-    street: string;
-    city:string;
-  };
 
-  hobbies: string[];
-  constructor(){
-    
+   users: string[]=['adrian', 'pedro','max', 'jose','rudy','francis'];
+   sayHello(){
+     alert("Hello");
+   };
+
+   deleteUser(user){
+      for (let index = 0; index < this.users.length; index++) 
+        {
+           if (this.users[index]==user)
+           {
+             this.users.splice(index,1)
+           }
+        }
+  } 
+
+    /*name: string='Ryan Ray';
+    age:number;
+    address:{
+      street: string;
+      city:string;
+    };
+
+    hobbies: string[];
+    constructor(){
+
     this.age=28;
     this.address={
       street:'calle 8 miami',
       city:'EEUU'
     };
     this.hobbies=['swimming','read','write'];
-  }; 
+  }; */
+
+
+
 
   
 
